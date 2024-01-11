6206ee3676c8da09c2f201b56652c40107f0169f
zacharylinorman <zacharylinorman@Zacharys-MacBook-Pro.local>
2024-01-11 09:24:17 -0800

    Update privacy policy

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -1,7 +1,10 @@
 export default function Privacy() {
 
     return(
-        <div>Seven Pixel Studios apps do not collect any user information.</div>
+        <div>
+            <div>Seven Pixel Studios apps do not collect any user information.</div>
+            <div>Seven Pixel Studios uses Google AdMob to serve ads.</div>
+        </div>
     );
 
 }
